efdb4f75728a03d32d9e7cc2a539f2d8f62cb128
Sajidk25 <sajidk25@gmail.com>
2019-05-31 01:54:57 +0600

    003 data bag encryption completed

--- cookbooks/mean-stack/recipes/mongodb.rb
+++ cookbooks/mean-stack/recipes/mongodb.rb
@@ -1,3 +1,5 @@
+secret=Chef::EncryptedDataBagItem.load_secret("/home/vagrant/.my_secret_key")
+credentials=Chef::EncryptedDataBagItem.load("credentials","credentials",secret)
 package "mongodb" do
     action :install
 end
@@ -10,23 +12,23 @@ template '/tmp/create_admin.js' do
     source 'create_admin.js.erb'
     action :create
     variables({
-        :admin_user     => node['db']['admin']['user'],
-        :admin_pass     => node['db']['admin']['password']
+        :admin_user     => credentials["db_admin_user"],
+        :admin_pass     => credentials["db_admin_password"]
     })
 end
 template '/tmp/create_app_user.js' do
     source 'create_app_user.js.erb'
     action :create
     variables({
-        :app_user     => node['db']['app']['user'],
-        :app_pass     => node['db']['app']['password']
+        :app_user     =>    credentials["db_app_user"],
+        :app_pass     =>    credentials["db_app_password"]
     })
 end
 template '/tmp/check_app_user.js' do
     source 'check_app_user.js.erb'
     action :create
     variables({
-        :app_user     => node['db']['app']['user'],
+        :app_user     => credentials["db_app_user"]
     })
 end
 
@@ -41,7 +43,13 @@ cookbook_file '/etc/mongodb.conf' do
     notifies:restart,"service[mongodb]"
 end
 execute 'Create the app user for the database' do
-    command "mongo  -u #{node['db']['admin']['user']} -p #{node['db']['admin']['password']} admin < /tmp/create_app_user.js"
+    command "mongo  -u #{credentials["db_app_user"]} -p #{credentials["db_admin_password"]} admin < /tmp/create_app_user.js"
     action :run
-    only_if { `mongo --quiet -u #{node['db']['admin']['user']} -p #{node['db']['admin']['password']} admin < /tmp/check_app_user.js`.include? "0" }
+    only_if { `mongo --quiet -u #{credentials["db_app_user"]} -p #{credentials["db_admin_password"]} admin < /tmp/check_app_user.js`.include? "0" }
+end
+
+[ '/tmp/create_admin.js','/tmp/create_app_user.js','/tmp/check_app_user.js'].each do | f |
+    file f do
+        action :delete
+    end
 end
